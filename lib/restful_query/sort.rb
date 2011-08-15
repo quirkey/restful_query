891bb68bf68a786314d3c5def1a8c3680a6169af
Aaron Quint <aaron@quirkey.com>
2011-08-15 17:08:03 -0400

    Sort takes an options hash for things like NULLS LAST

--- lib/restful_query/sort.rb
+++ lib/restful_query/sort.rb
@@ -1,11 +1,11 @@
 module RestfulQuery
   class InvalidDirection < Error; end
-  
+
   class Sort
     include Comparable
-    
-    attr_reader :column, :direction
-    
+
+    attr_reader :column, :direction, :options
+
     DIRECTIONS = {
       'up' => 'ASC',
       'asc' => 'ASC',
@@ -14,37 +14,51 @@ module RestfulQuery
       'desc' => 'DESC',
       'DESC' => 'DESC'
     }.freeze
-    
-    
-    def initialize(column, direction)
+
+
+    def initialize(column, direction, options = {})
       self.column    = column
       self.direction = direction
+      self.options = options
     end
-    
+
     def self.parse(sort_string, split_on = /\-|\ /)
       return unless sort_string
-      column, direction = sort_string.split(split_on)
-      new(column, direction)
+      column, direction, options = sort_string.split(split_on, 3)
+      new(column, direction, options)
     end
-    
+
     def column=(column)
       @column = column.to_s
     end
-    
+
     def direction=(direction)
       @direction = DIRECTIONS[direction.to_s]
       raise(InvalidDirection, "'#{direction}' is not a valid order direction") unless @direction
     end
-    
+
+    def options=(options)
+      @options = if options.is_a?(String)
+        opts = {}
+        options.split(/ /).each_slice(2) do |k|
+          k.map! {|v| v.to_s.downcase.to_sym }
+          opts[k[0]] = k[1]
+        end
+        opts
+      else
+        options || {}
+      end
+    end
+
     def reverse_direction
       direction == 'ASC' ? 'DESC' : 'ASC'
     end
-    
+
     def ==(other)
       return false unless other.is_a?(Sort)
       column == other.column && direction == other.direction
     end
-    
+
     # Makes a roundabout for directions nil -> desc -> asc -> nil
     def self.next_direction(current_direction)
       case current_direction.to_s.downcase
@@ -56,17 +70,23 @@ module RestfulQuery
         'desc'
       end
     end
-    
+
     def next_direction
       self.class.next_direction(direction)
     end
-    
+
     def to_s(join = '-')
-      "#{column}#{join}#{direction.downcase}"
+      s = "#{column}#{join}#{direction.downcase}"
+      s << " #{options.inspect}" unless options.empty?
+      s
     end
-            
+
     def to_sql
-      "#{column} #{direction}"
+      sql = "#{column} #{direction}"
+      unless options.empty?
+        sql << ' ' << options.to_a.flatten.collect {|k| k.to_s.upcase }.join(' ')
+      end
+      sql
     end
   end
 end
--- test/test_restful_query_sort.rb
+++ test/test_restful_query_sort.rb
@@ -8,16 +8,34 @@ class RestfulQuerySortTest < Test::Unit::TestCase
         setup do
           @sort = RestfulQuery::Sort.new(:attribute, 'up')
         end
-        
+
         should "save column name as string" do
           assert_equal 'attribute', @sort.column
         end
-        
+
         should "interpret direction" do
           assert_equal 'ASC', @sort.direction
         end
       end
-      
+
+      context "with extra options" do
+        setup do
+          @sort = RestfulQuery::Sort.new(:attribute, 'up', :nulls => :first)
+        end
+
+        should "save column name as string" do
+          assert_equal 'attribute', @sort.column
+        end
+
+        should "interpret direction" do
+          assert_equal 'ASC', @sort.direction
+        end
+
+        should "add nulls option" do
+          assert_equal :first, @sort.options[:nulls]
+        end
+      end
+
       context "with an invalid direction" do
         should "raise error" do
           assert_raise(RestfulQuery::InvalidDirection) do
@@ -25,28 +43,28 @@ class RestfulQuerySortTest < Test::Unit::TestCase
           end
         end
       end
-      
+
     end
-    
+
     context "parse" do
       context "with a query hash like condition" do
       setup do
         @sort = RestfulQuery::Sort.parse('long_name_attribute-down')
       end
-      
+
       should "return sort object" do
         assert @sort.is_a?(RestfulQuery::Sort)
       end
-      
+
       should "set column and direction" do
         assert_equal 'long_name_attribute', @sort.column
         assert_equal 'DESC', @sort.direction
       end
       end
-      
+
       context "with a standard SQL like condition" do
         setup do
-          @sort = RestfulQuery::Sort.parse('long_name_attribute DESC')
+          @sort = RestfulQuery::Sort.parse('long_name_attribute DESC NULLS LAST')
         end
 
         should "return sort object" do
@@ -57,16 +75,25 @@ class RestfulQuerySortTest < Test::Unit::TestCase
           assert_equal 'long_name_attribute', @sort.column
           assert_equal 'DESC', @sort.direction
         end
+
+        should "set other options" do
+          assert_equal :last, @sort.options[:nulls]
+        end
       end
     end
-    
+
     context "to_sql" do
       should "join the column and attribute" do
         @sort = RestfulQuery::Sort.new(:attribute, 'down')
         assert_equal 'attribute DESC', @sort.to_sql
       end
+
+      should "parse null option" do
+        @sort = RestfulQuery::Sort.new(:attribute, 'down', :nulls => :last)
+        assert_equal 'attribute DESC NULLS LAST', @sort.to_sql
+      end
     end
-    
+
   end
-  
+
 end
