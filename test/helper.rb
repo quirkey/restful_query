1ec2435a0702feab30f56f278b804f783d621229
arirusso <ari.russo@gmail.com>
2015-02-25 14:06:47 -0500

    use shoulda-context instead of full shoulda

--- test/helper.rb
+++ test/helper.rb
@@ -2,7 +2,7 @@ $LOAD_PATH.unshift(File.join(File.expand_path(File.dirname(__FILE__)), "../lib")
 
 require "rubygems"
 require "minitest/autorun"
-require "shoulda"
+require "shoulda-context"
 
 require "restful_query"
 
